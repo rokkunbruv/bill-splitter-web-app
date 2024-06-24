5fe30623fc1807b38f457d77802757b66c1fe30b
27rhaner <eroja.rhanzel@gmail.com>
2024-06-24 22:19:27 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://rhanzeleroja:rhanzeleroja123@bbc-mern.ecxfqho.mongodb.net/BBCproject?retryWrites=true&w=majority&appName=BBC-MERN'
