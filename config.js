a00376ec6fd3a6cbcdf681a681a6fc543688f478
rokkunbruv <themorghiknight579@gmail.com>
2024-06-25 19:28:41 +0800

    integrated database into repo

--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://rhanzeleroja:rhanzeleroja123@bbc-mern.ecxfqho.mongodb.net/BBCproject?retryWrites=true&w=majority&appName=BBC-MERN'
--- server/index.js
+++ server/index.js
@@ -2,19 +2,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to our web app!');
+    console.log(req);
+    return response.status(234).send('Welcome to Bill Splitter App');
 })
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5555;
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-})
+mongoose
+    .connect(process.env.CONNECTION_URL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
