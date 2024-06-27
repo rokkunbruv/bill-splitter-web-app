e3627cc992ce0a1845794aa01158b5f95b86c608
conxant0 <confox2145@gmail.com>
2024-06-27 17:08:33 +0800

    added skeleton frontend for uploading receipt

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const Tesseract = require('tesseract.js');
+const fs = require('fs');
+
+const app = express();
+const port = 5555;
+
+ES6
+
 
 import express from 'express';
+import bodyParser from 'body-parser';
 
