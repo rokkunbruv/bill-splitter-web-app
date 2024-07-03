b58f0a866a7cfbc71ee8839d3cfe9a88a9c95852
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-07-03 20:00:37 +0800

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,4 @@ export const fetchReceipts = () => axios.get(url);
 export const createReceipt = (newReceipt) => axios.post(url, newReceipt);
 export const updateReceipt = (id, updatedReceipt) => axios.patch(`${url}/${id}`, updatedReceipt);
 export const deleteReceipt = (id) => axios.delete(`${url}/${id}`);
+export const assignItemToUser = (receiptId, user, item) => axios.patch(`${url}/${receiptId}/assign`, { user, item });
