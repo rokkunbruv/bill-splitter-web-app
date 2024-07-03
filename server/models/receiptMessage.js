8e7707bfef18172249f3635eefce7e5335f364cc
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-07-03 20:07:10 +0800

    Update receiptMessage.js

--- server/models/receiptMessage.js
+++ server/models/receiptMessage.js
@@ -1,12 +1,30 @@
 import mongoose from 'mongoose';
 
+const itemSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    quantity: Number
+});
+
+const userWithItemsSchema = new mongoose.Schema({
+    userName: String,
+    items: [itemSchema],
+    bill: {
+        type: Number,
+        default: 0
+    }
+});
+
 const receiptSchema = mongoose.Schema({
     event: String,
     users: [ String ],
     uploadedFile: String,
     gptResponse: mongoose.Schema.Types.Mixed,
+    gptCopy: mongoose.Schema.Types.Mixed,
+    usersWithItems: [userWithItemsSchema],
 });
 
 const ReceiptMessage = mongoose.model('ReceiptMessage', receiptSchema);
 
 export default ReceiptMessage;
+
