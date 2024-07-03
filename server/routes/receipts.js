76c0fbe49287deb6c8aca98b78b49df77474f4f9
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-07-03 20:07:50 +0800

    Update receipts.js

--- server/routes/receipts.js
+++ server/routes/receipts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getReceipts, createReceipt, updateReceipt, deleteReceipt } from '../controllers/receipts.js';
+import { getReceipts, createReceipt, updateReceipt, deleteReceipt, assignItemToUser} from '../controllers/receipts.js';
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.get('/', getReceipts);
 router.post('/', createReceipt);
 router.patch('/:id', updateReceipt);
 router.delete('/:id', deleteReceipt);
+router.patch('/:id/assign', assignItemToUser)
+
 
 export default router;
